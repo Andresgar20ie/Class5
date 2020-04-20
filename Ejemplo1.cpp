79f3f9e4a8d977576e8f17b46c1021a77991916d
Andresgar20ie <andres.garcia-ro@mail.escuelaing.edu.co>
2020-04-20 13:22:31 +0000

    se añaden archivos iniciales

---
+++ Ejemplo1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	cout<<"La cantidad de argumentos de entrada son:"<<argc<<endl;
+	
+	return 0;
+}
+
+void funcion1()
+{
+	int valor1 = 0;
+}
+
+
+//std::cout----- :: Scope Resolution Operador (Operador de alcance)
+/*
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	std::<<"La cantidad de argumentos de entrada son:"<<
+	argc<<end1;
+	
+	return 0;
+}
+
+void funcion1()
+{
+	int valor1 = 0;
+}
+*/
